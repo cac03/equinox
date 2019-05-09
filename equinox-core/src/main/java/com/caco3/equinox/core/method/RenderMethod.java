fad47a36d095efce184eb9504e78ee195e28156d
Denis Zavedeev <happy96538@gmail.com>
2019-05-10 00:57:10 +0300

    Introduce RenderMethod wrapper
    RenderMethod is a wrapper for actual method performing rendering
    Refer to javadoc for more information

---
+++ equinox-core/src/main/java/com/caco3/equinox/core/method/RenderMethod.java
@@ -0,0 +1,72 @@
+package com.caco3.equinox.core.method;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.core.annotation.SynthesizingMethodParameter;
+import org.springframework.lang.Nullable;
+import org.springframework.util.Assert;
+import org.springframework.util.ReflectionUtils;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.Parameter;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
+
+/**
+ * Convenient wrapper for actual render method.
+ */
+public class RenderMethod {
+  /**
+   * The target method
+   */
+  private final Method method;
+
+  public RenderMethod(Method method) {
+    Assert.notNull(method, "method == null");
+    this.method = method;
+  }
+
+  /**
+   * Return list target method parameters as {@link MethodParameter}s
+   * The returned list is read-only
+   *
+   * @return target method parameters
+   */
+  public List<MethodParameter> getParameters() {
+    return Arrays.stream(method.getParameters())
+        .map(RenderMethod::toMethodParameter)
+        .collect(collectingAndThen(toList(), Collections::unmodifiableList));
+  }
+
+  private static MethodParameter toMethodParameter(Parameter parameter) {
+    return SynthesizingMethodParameter.forParameter(parameter);
+  }
+
+  /**
+   * Invoke target method on given {@code target} with {@code args}
+   *
+   * @param target to invoke method on
+   * @param args   to invoke method with
+   * @return return value of target method
+   * @throws IllegalArgumentException if {@code args == null}
+   * @throws IllegalArgumentException if target method is not static
+   *                                  but {@code target} is {@code null}
+   */
+  public Object invoke(@Nullable Object target, Object... args) {
+    Assert.notNull(args, "args == null");
+    if (target == null) {
+      Assert.isTrue(Modifier.isStatic(method.getModifiers()),
+          () -> "target == null but method = \"" + method + "\" is not static");
+    }
+    return ReflectionUtils.invokeMethod(method, target, args);
+  }
+
+  @Override
+  public String toString() {
+    return "RenderMethod(targetMethod=" + method + ")";
+  }
+}
---
+++ equinox-core/src/test/java/com/caco3/equinox/core/method/RenderMethodTest.kt
@@ -0,0 +1,69 @@
+package com.caco3.equinox.core.method
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.springframework.web.bind.annotation.ModelAttribute
+import kotlin.reflect.jvm.javaMethod
+
+class RenderMethodTest {
+    fun dummyMethod(
+            parameter1: String,
+            @ModelAttribute("string") annotatedParameter1: String
+    ) {
+    }
+
+    class GetParametersTests {
+
+        private val renderMethod = RenderMethod(RenderMethodTest::dummyMethod.javaMethod!!)
+
+        @Test
+        fun `should return 2 parameters`() {
+            assertEquals(2, renderMethod.parameters.size)
+        }
+
+        @Test
+        fun `@AliasFor kicks in when reading annotations`() {
+            val annotation = renderMethod.parameters[1].getParameterAnnotation(ModelAttribute::class.java)
+            assertEquals("string", annotation!!.name)
+        }
+
+        @Test
+        fun `returned list is read only`() {
+            val mutableList = renderMethod.parameters
+            assertThrows<UnsupportedOperationException> {
+                mutableList.removeAt(0)
+            }
+        }
+    }
+
+    @Test
+    fun `descriptive toString returned`() {
+        val javaMethod = ::dummyMethod.javaMethod!!
+        val toString = RenderMethod(javaMethod).toString()
+        assertEquals("RenderMethod(targetMethod=$javaMethod)", toString)
+    }
+
+    fun returnFirstParameter(any: Any): Any = any
+
+    @Test
+    fun `invoke dummy method`() {
+        val renderMethod = RenderMethod(::returnFirstParameter.javaMethod!!)
+        val returnValue = renderMethod.invoke(this, true)
+        assertEquals(true, returnValue)
+    }
+
+    @Test
+    fun `constructor throws IllegalArgumentException if method is null`() {
+        assertThrows<IllegalArgumentException> {
+            RenderMethod(null)
+        }
+    }
+
+    @Test
+    fun `throws IllegalArgumentException if method is not static and target is null`() {
+        assertThrows<IllegalArgumentException> {
+            RenderMethod(::returnFirstParameter.javaMethod!!).invoke(null, 42)
+        }
+    }
+}
